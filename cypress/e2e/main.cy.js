c89ea3478c818fd4664b23e11274c97d05a48919
fjuza1 <104192489+fjuza1@users.noreply.github.com>
2024-05-21 14:31:19 +0200

    n

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -4,14 +4,15 @@ describe('We can search ',()=>{
         cy.get('.col-lg-3.show > .input-items > .form-floating > .select2 > .selection > .select2-selection > .select2-selection__arrow > b').click();
         // fisrt select from destination
         cy.get('#select2--results > div').children().then((des1)=>{
-            const randomOption = Math.floor(Math.random()*des1.length)
+            const randomOption = Math.floor(Math.random()*des1.length)+1
             cy.get(`.most--popular-from > :nth-child(${randomOption})`).click()
         })
         cy.get('.mt-2.show.active').last().click()
         cy.get('.most--popular-to').children().then((des2)=>{
-            const randomOption = Math.floor(Math.random()*des2.length)
+            const randomOption = Math.floor(Math.random()*des2.length)+1
             cy.get(`.most--popular-to > :nth-child(${randomOption})`).click()
         })
-        cy.get('#departure[value = "24-05-2024"]').type()
+        cy.get('#departure[value = "24-05-2024"]').type('{selectall}{backspace}24-05-2024')
+        cy.get('.col-lg-1 > #flights-search').click();
     })
 })
