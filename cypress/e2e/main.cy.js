10afb07a58ac46a1c84bf7e8a0a934e3be158614
fjuza1 <104192489+fjuza1@users.noreply.github.com>
2024-05-22 13:00:08 +0200

    in helpers.js

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,20 +1,4 @@
-const {futureDate} = require('../../helpers.js');
-const fillSearchData = function(){
-    let randomOption
-    cy.visit(Cypress.config('PHPTRAVELS'))
-    cy.get('.col-lg-3.show > .input-items > .form-floating > .select2 > .selection > .select2-selection > .select2-selection__arrow > b').click();
-    // fisrt select from destination
-    cy.get('#select2--results > div').children().then((des1)=>{
-        randomOption = Math.floor(Math.random()*des1.length)+1
-        cy.get(`.most--popular-from > :nth-child(${randomOption})`).click()
-    })
-    cy.get('.mt-2.show.active').last().click()
-    cy.get('.most--popular-to').children().then((des2)=>{
-        randomOption = Math.floor(Math.random()*des2.length)+1
-        cy.get(`.most--popular-to > :nth-child(${randomOption})`).click()
-    })
-    cy.get('#departure').type(`{selectall}{backspace}${futureDate()}`)
-}
+const {futureDate, fillSearchData} = require('../../helpers.js');
 describe('We can search ',()=>{
     it('search destination', function () {
         cy.continueOnFail()
@@ -25,7 +9,7 @@ describe('We can search ',()=>{
         })
 
         cy.get('.col-lg-1 > #flights-search').click();
-        cy.get('#flights--list-js').next().children().as('searchedFlightsList')
+        cy.get('#flights--list-js').next().next().children().as('searchedFlightsList')
         console.log(cy.get('@searchedFlightsList'));
     })
     it('We get no flights',function() {
--- helpers.js
+++ helpers.js
@@ -10,4 +10,20 @@ const futureDate = ()=>{
     const dateRand = new Intl.DateTimeFormat('sk-SK',options).format(date).replace(/\./g, '-')
     return dateRand
   }
-  module.exports = {futureDate}
+  const fillSearchData = function(){
+    let randomOption
+    cy.visit(Cypress.config('PHPTRAVELS'))
+    cy.get('.col-lg-3.show > .input-items > .form-floating > .select2 > .selection > .select2-selection > .select2-selection__arrow > b').click();
+    // fisrt select from destination
+    cy.get('#select2--results > div').children().then((des1)=>{
+        randomOption = Math.floor(Math.random()*des1.length)+1
+        cy.get(`.most--popular-from > :nth-child(${randomOption})`).click()
+    })
+    cy.get('.mt-2.show.active').last().click()
+    cy.get('.most--popular-to').children().then((des2)=>{
+        randomOption = Math.floor(Math.random()*des2.length)+1
+        cy.get(`.most--popular-to > :nth-child(${randomOption})`).click()
+    })
+    cy.get('#departure').type(`{selectall}{backspace}${futureDate()}`)
+}
+  module.exports = {futureDate,fillSearchData}
